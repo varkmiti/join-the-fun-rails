9de762f49b61c460ff5f9ad6a31edf24fbb3baf5
Mark Viti <+>
2022-06-27 09:56:59 -0400

    commit tree c42435d63e3fbb964530ffe103d58827498883ef

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration[5.0]
   def change
     create_table :rides do |t|
+      t.integer :taxi_id
+      t.integer :passenger_id
       t.timestamps null: false
     end
   end
